c7967a1bde7941c48812872621ab0f16427b7c46
Ahmad <ahmad@iqreateinfotech.com>
2019-10-14 17:59:31 +0530

    frist

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,236 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="sandbox">
+    <v-navigation-drawer
+            v-model="primaryDrawer.model"
+            :clipped="primaryDrawer.clipped"
+            :floating="primaryDrawer.floating"
+            :mini-variant="primaryDrawer.mini"
+            :permanent="primaryDrawer.type === 'permanent'"
+            :temporary="primaryDrawer.type === 'temporary'"
+            app
+            overflow
+    >
+      <v-list dense>
+        <template v-for="item in items">
+          <v-row
+                  v-if="item.heading"
+                  :key="item.heading"
+                  align="center"
+          >
+            <v-col cols="6">
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-col>
+            <v-col
+                    cols="6"
+                    class="text-center"
+            >
+              <a
+                      href="#!"
+                      class="body-2 black--text"
+              >EDIT</a>
+            </v-col>
+          </v-row>
+          <v-list-group
+                  v-else-if="item.children"
+                  :key="item.text"
+                  v-model="item.model"
+                  :prepend-icon="item.model ? item.icon : item['icon-alt']"
+                  append-icon=""
+          >
+            <template v-slot:activator>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>
+                    {{ item.text }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+            <v-list-item
+                    v-for="(child, i) in item.children"
+                    :key="i"
+                    @click=""
+            >
+              <v-list-item-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ child.text }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+          <v-list-item
+                  v-else
+                  :key="item.text"
+                  @click=""
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+            :clipped-left="primaryDrawer.clipped"
+            app
+    >
+      <v-app-bar-nav-icon
+              v-if="primaryDrawer.type !== 'permanent'"
+              @click.stop="primaryDrawer.model = !primaryDrawer.model"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <v-row
+                align="center"
+                justify="center"
+        >
+          <v-col cols="10">
+            <v-card>
+              <v-card-text>
+                <v-row>
+                  <v-col
+                          cols="12"
+                          md="6"
+                  >
+                    <span>Scheme</span>
+                    <v-switch
+                            v-model="$vuetify.theme.dark"
+                            primary
+                            label="Dark"
+                    ></v-switch>
+                  </v-col>
+                  <v-col
+                          cols="12"
+                          md="6"
+                  >
+                    <span>Drawer</span>
+                    <v-radio-group
+                            v-model="primaryDrawer.type"
+                            column
+                    >
+                      <v-radio
+                              v-for="drawer in drawers"
+                              :key="drawer"
+                              :label="drawer"
+                              :value="drawer.toLowerCase()"
+                              primary
+                      ></v-radio>
+                    </v-radio-group>
+                    <v-switch
+                            v-model="primaryDrawer.clipped"
+                            label="Clipped"
+                            primary
+                    ></v-switch>
+                    <v-switch
+                            v-model="primaryDrawer.floating"
+                            label="Floating"
+                            primary
+                    ></v-switch>
+                    <v-switch
+                            v-model="primaryDrawer.mini"
+                            label="Mini"
+                            primary
+                    ></v-switch>
+                  </v-col>
+                  <v-col
+                          cols="12"
+                          md="6"
+                  >
+                    <span>Footer</span>
+                    <v-switch
+                            v-model="footer.inset"
+                            label="Inset"
+                            primary
+                    ></v-switch>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+              <v-card-actions>
+                <div class="flex-grow-1"></div>
+                <v-btn text>Cancel</v-btn>
+                <v-btn
+                        text
+                        color="primary"
+                >Submit</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer
+            :inset="footer.inset"
+            app
+    >
+      <span class="px-4">&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+  export default {
+    data: () => ({
+      drawers: ['Default (no property)', 'Permanent', 'Temporary'],
+      primaryDrawer: {
+        model: null,
+        type: 'default (no property)',
+        clipped: false,
+        floating: false,
+        mini: false,
+      },
+      footer: {
+        inset: false,
+      },
+      items: [
+        { icon: 'contacts', text: 'Contacts' },
+        { icon: 'history', text: 'Frequently contacted' },
+        { icon: 'content_copy', text: 'Duplicates' },
+        {
+          icon: 'keyboard_arrow_up',
+          'icon-alt': 'keyboard_arrow_down',
+          text: 'Labels',
+          model: true,
+          children: [
+            { icon: 'add', text: 'Create label' },
+          ],
+        },
+        {
+          icon: 'keyboard_arrow_up',
+          'icon-alt': 'keyboard_arrow_down',
+          text: 'More',
+          model: false,
+          children: [
+            { text: 'Import' },
+            { text: 'Export' },
+            { text: 'Print' },
+            { text: 'Undo changes' },
+            { text: 'Other contacts' },
+          ],
+        },
+        { icon: 'settings', text: 'Settings' },
+        { icon: 'chat_bubble', text: 'Send feedback' },
+        { icon: 'help', text: 'Help' },
+        { icon: 'phonelink', text: 'App downloads' },
+        { icon: 'keyboard', text: 'Go to the old version' },
+      ],
+    }),
+    created() {
+      this.drawers = this.$store.state.drawers;
     }
   }
-}
-</style>
+</script>
---
+++ src/store/modules/layout.js
@@ -0,0 +1,24 @@
+const state = {
+    drawers: ['Default (no property)', 'Permanent', 'Temporary'],
+    primaryDrawer: {
+        model: null,
+        type: 'default (no property)',
+        clipped: false,
+        floating: false,
+        mini: false,
+    },
+    footer: {
+        inset: false,
+    },
+};
+
+const getters = {};
+const mutations = {};
+const actions = {};
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions,
+}
